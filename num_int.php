2b2b4555617a77c1235b7c15a2c134955c322e26
Jajang-man-bungbung <91002280+RejikaParbosa@users.noreply.github.com>
2022-06-03 11:31:30 +0300

    Integer Number

---
+++ num_int.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Check if the type of a variable is integer   
+$x = 59.85;
+var_dump(is_int($x));
+
+echo "<br>";
+
+// Check again... 
+$x = 5454;
+var_dump(is_int($x));
+?>  
+
+</body>
+</html>
+
